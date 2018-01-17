27ee17bcfadcc4a5776bedf870c5b8cbad49b983
zongrong <ssszwzr@126.com>
2018-01-17 21:36:17 +0800

    update

--- dist/interfaces/wx.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInd4LmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIiLCJmaWxlIjoid3guanMiLCJzb3VyY2VzQ29udGVudCI6W119
